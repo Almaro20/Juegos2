dc42d5d0cff115f1d719182614dbcbb0cdbc51f5
Almaro20 <amr0100@alu.medac.es>
2025-02-10 16:00:37 +0100

    clave

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "TU_API_KEY"; // Reemplaza con tu clave de RAWG
+const API_KEY = process.env.REACT_APP_RAWG_API_KEY;
 const BASE_URL = "https://api.rawg.io/api";
 
 export const getPopularGames = async () => {
