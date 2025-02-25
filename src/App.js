e9bfa461bcde9f24f5107f6c83ec02b38ec0e9e6
Almaro20 <amr0100@alu.medac.es>
2025-02-25 21:46:08 +0100

    detalles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';  // La página de inicio
 import GamesPage from './pages/GamesPage';  // La página de juegos
+import GameDetailPage from './pages/GameDetailPage';
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -16,6 +19,9 @@ function App() {
         
         {/* Ruta de todos los juegos */}
         <Route path="/games" element={<GamesPage />} />
+
+        <Route path="/games/:id" element={<GameDetailPage />} />
+
       </Routes>
     </Router>
   );
